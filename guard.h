8268d18c89a5219ce6a12aad5c057e6ade91d3fc
Cassio Koshikumo <ckoshikumo@gmail.com>
2025-08-04 04:10:38 -0300

    Initial commit
    Actually this was developed in another (local) git repo, home to various small
    libs. But then I decided to give each its own repo, and there's no way I'm going
    to wrestle with git to get the history for each one. So here we are.

---
+++ guard.h
@@ -0,0 +1,47 @@
+#ifndef INCLUDE_GUARD_H
+#define INCLUDE_GUARD_H
+
+#include <errno.h>
+#include <stdlib.h>
+
+#define guard(a, msg, ...)                                                     \
+	if (!(a)) {                                                                \
+		rec_error(msg __VA_OPT__(, ) __VA_ARGS__);                            \
+		errno = 0;                                                             \
+		goto error;                                                            \
+	}
+
+#define guard_debug(a, msg, ...)                                               \
+	if (!(a)) {                                                                \
+		rec_debug(msg __VA_OPT__(, ) __VA_ARGS__);                            \
+		errno = 0;                                                             \
+		goto error;                                                            \
+	}
+
+#define guard_log(a, msg, ...)                                                 \
+	if (!(a)) {                                                                \
+		rec_log(msg __VA_OPT__(, ) __VA_ARGS__);                              \
+		errno = 0;                                                             \
+		goto error;                                                            \
+	}
+
+#define guard_mem(a)                                                           \
+	if (!(a)) {                                                                \
+		rec_error("Memory error");                                             \
+		exit(EXIT_FAILURE);                                                    \
+	}
+
+#define guard_exit(a, msg, ...)                                                \
+	if (!(a)) {                                                                \
+		rec_error(msg __VA_OPT__(, ) __VA_ARGS__);                            \
+		exit(EXIT_FAILURE);                                                    \
+	}
+
+#define guard_sentinel(msg, ...)                                               \
+	{                                                                          \
+		rec_error(msg __VA_OPT__(, ) __VA_ARGS__);                            \
+		errno = 0;                                                             \
+		goto error;                                                            \
+	}
+
+#endif // INCLUDE_GUARD_H
